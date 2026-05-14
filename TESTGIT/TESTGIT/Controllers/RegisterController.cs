a27c2538f6bb5deac666ec2a5a80fec3bed624e3
dayana179 <faiqahdayana04@gmail.com>
2026-05-14 20:02:23 +0800

    create registerview

---
+++ TESTGIT/TESTGIT/Controllers/RegisterController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TESTGIT.Models;
+
+namespace TESTGIT.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = "Registration Successful!";
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ TESTGIT/TESTGIT/Models/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TESTGIT.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Email format is not valid.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Confirm Email is required.")]
+        [Compare("Email", ErrorMessage = "Email and Confirm Email must match.")]
+        public string ConfirmEmail { get; set; }
+
+        [Required(ErrorMessage = "Age is required.")]
+        [Range(18, 60, ErrorMessage = "Age must be between 18 and 60.")]
+        public int? Age { get; set; }
+
+        [Range(typeof(bool), "true", "true", ErrorMessage = "You must agree to the terms.")]
+        public bool AgreeTerms { get; set; }
+    }
+}
