b40cbd5316b26a303abde0e4558a55618f8bea40
dayana179 <faiqahdayana04@gmail.com>
2026-05-14 15:18:41 +0800

    The first test

---
+++ TESTGIT/TESTGIT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TESTGIT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
